dc7af64b681f2ae532a62e7ae0ae55c1d0c2671a
lightningstrikeee <xenderborn3003@gmail.com>
2024-11-24 18:15:27 +0700

    Fix

--- test_calculator.py
+++ test_calculator.py
@@ -7,24 +7,24 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(1, 2), 3)
-        self.assertEqual(self.calc.add(3, 2), 5)
+        self.assertEqual(self.calc.add(5, 7), 12)
+        self.assertEqual(self.calc.add(8, 3), 11)
 
     def test_subtract(self):
-        self.assertEqual(self.calc.subtract(2, 1), 1)
-        self.assertEqual(self.calc.subtract(3, 2), 1)
+        self.assertEqual(self.calc.subtract(6, 4), 2)
+        self.assertEqual(self.calc.subtract(10, 6), 4)
     
     def test_multiply(self):
-        self.assertEqual(self.calc.multiply(2, 3), 6)
-        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(6, 7), 42)
     
     def test_divide(self):
-        self.assertEqual(self.calc.divide(6, 2), 3)
-        self.assertEqual(self.calc.divide(9, 3), 3)
+        self.assertEqual(self.calc.divide(8, 4), 2)
+        self.assertEqual(self.calc.divide(12, 3), 4)
 
     def test_modulo(self):
-        self.assertEqual(self.calc.modulo(5, 2), 1)
-        self.assertEqual(self.calc.modulo(10, 3), 1)
+        self.assertEqual(self.calc.modulo(7, 3), 1)
+        self.assertEqual(self.calc.modulo(15, 4), 3)
 
     # Add the following test methods to the TestCalculator class:
 
